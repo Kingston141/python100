06796cf0108e19d743a4297f9e1b651286721297
Kingston141 <kingkingston565@gmail.com>
2025-08-20 19:57:49 +0530

    commmitted

---
+++ day 12/find the second largest number in list.py
@@ -0,0 +1,11 @@
+list=[2,6,12,1,56,90]
+list.sort()
+print(list)
+print(f"the second largest number in list {list[-2]}")
+
+'''list=[2,6,12,1,56,90]
+largest = max(list)
+list.remove(largest)
+second_largest = max(list)
+print("The second largest is:", second_largest)
+'''
